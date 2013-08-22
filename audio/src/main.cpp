a2cba07731322cfe9002b4ef519d26db5259ca82
Michael Maslyukov <m.maslyukov@gmail.ru>
2013-08-22 09:30:07 +0300

    Firts step to play pcm from file

--- audio/src/main.cpp
+++ audio/src/main.cpp
@@ -7,6 +7,7 @@
 #include <audio/Audio.h>
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 static const char hello[] = {
 #include <samples/hello_clip.h>
@@ -17,14 +18,36 @@ static const char android[] = {
         };
 
 using namespace std;
+//    std::fstream out("hello.pcm", std::ios_base::out);
+//    for (int i = 0; i < sizeof(hello); ++i) {
+//        out << hello[i];
+//    }
+//    out.close();
 
-int main() {
+int main(int argc, char* argv[]) {
+
+//    if (argc != 2) {
+//        cout << "use with PCM file\n";
+//        return -1;
+//    }
+    string input(argv[1]);
+
+
+
+    std::fstream in(input, std::ios_base::in);
     const vector<short> pcm1((short*)&android[0], (short*)&android[sizeof(android) - 1]);
-    const vector<short> pcm2((short*)hello, (short*)&hello[sizeof(hello) - 1]);
+//    const vector<short> pcm2((short*)hello, (short*)&hello[sizeof(hello) - 1]);
+    vector<char> pcm;
+    char temp;
+    while (!in.fail()) {
+        in >> temp;
+        pcm.push_back(temp);
+    }
+//    const vector<short> pcm1(pcm.begin(), pcm.end());
+
     cout << "m1" << endl;
     OpenSL::Audio a;
     a.add(0,pcm1);
-    a.add(1,pcm2);
     a.play();
     cout << "m2" << endl;
     while (1) {
